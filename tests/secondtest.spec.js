a7283c9ee317ca138badd2771a54cc180cd37318
arunrajsqa <arunrajsqa@gmail.com>
2026-03-24 18:47:55 +0530

    Update secondtest.spec.js

--- tests/secondtest.spec.js
+++ tests/secondtest.spec.js
@@ -3,6 +3,6 @@ const{test,expect} =require('@playwright/test')
 test("My second test", async function({page})
 {
   console.log("Hello yaar this is my second test case");         
-    expect(100).toBe(500);
+    expect(100).toBe(500)/;
 
 });
