9d2cca46b1ba9f57e283fcb17d09ee9e11e4e49d
Arunraj <arunrajsqa@gmail.com>
2026-03-24 16:46:22 +0530

    new file was added

---
+++ tests/secondtest.spec.js
@@ -0,0 +1,8 @@
+const{test,expect} =require('@playwright/test')
+
+test("My second test", async function({page})
+{
+  console.log("Hello yaar this is my second test case");         
+    expect(100).toBe(500);
+
+});
